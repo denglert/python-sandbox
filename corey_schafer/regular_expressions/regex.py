c09f7f1016affb32f3d6b9c79548689266b01fa3
denglert <englert.david@gmail.com>
2017-12-18 20:02:06 +0000

    Started lesson on regular expressions.

---
+++ corey_schafer/regular_expressions/regex.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+import re
+
+abc_lc = "abcdefghijklmnopqrstuvwxyz"
+abc_uc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+nos = "1234567890"
+
+
+string = "Ha HaHa"
+
+#MetaCharacters ()
+
+print( re.search('abc', abc_lc))
